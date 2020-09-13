c77e3c416530e4aecf64017a92c6789bc64abd2a
blazeris <blazeris@users.noreply.github.com>
2020-09-12 18:31:52 -0700

    lab3 problem 2

--- Lab2/Main.cpp
+++ Lab2/Main.cpp
@@ -38,7 +38,6 @@ int main(){
    	cout << endl;
  
  
- 
  	//PROBLEM 2: Testing computing doubled investment main function
  	cout << "Enter initial investment amount to find time required to double amount: ";
  	int investment;
@@ -49,22 +48,20 @@ int main(){
  	
  	cout << endl;
  
-
  
  	//PROBLEM 3: Testing bank class
  	Bank my_bank;
 	cout << "Inital bank balances: \n";
 	my_bank.print_balances(); /* set up empty accounts */
-
 	cout << "Adding some money to accounts: \n";
-	my_bank.deposit(1000, "S"); /* deposit $1000 to savings */
-	my_bank.deposit(2000, "C"); /* deposit $2000 to checking */
+	my_bank.deposit(1000, "S"); // deposit $1000 to savings
+	my_bank.deposit(2000, "C"); // deposit $2000 to checking
 	my_bank.print_balances();
 	
 	cout << "Taking out $1500 from checking,and moving $200 from";
 	cout << " savings to checking.\n";
-	my_bank.withdraw(1500, "C"); /* withdraw $1500 from checking */
-	my_bank.transfer(200, "S"); /* transfer $200 from savings */
+	my_bank.withdraw(1500, "C"); // withdraw $1500 from checking
+	my_bank.transfer(200, "S"); // transfer $200 from savings
 	my_bank.print_balances();
 	
 	cout << "Trying to transfer $900 from Checking.\n";
@@ -74,7 +71,6 @@ int main(){
 	cout << "trying to transfer $900 from Savings.\n";
 	my_bank.transfer(900,"S");
 	my_bank.print_balances();
- 	
    return 0;  
 }
 
--- Lab3/Lab3.cpp
+++ Lab3/Lab3.cpp
@@ -7,6 +7,7 @@
 
 using namespace std;
 
+//Reads data from a text file and adds it to array using pointers
 int readData(int * &arr){
 	int size;
 	ifstream inputFile;
@@ -19,6 +20,7 @@ int readData(int * &arr){
 	return size;
 }
 
+//Bubble sort algorithm sorting array from pointer
 void bsort(int *arr, int last){
 	bool sorted = false;
 	while(!sorted){
@@ -34,20 +36,74 @@ void bsort(int *arr, int last){
 	}
 }
 
+//Prints the array to console
 void writeToConsole(int *arr, int last){
 	cout << "Sorted array:";
 	for(int i = 0; i < last; i++){
 		cout << " " << *(arr + i);
 	}
+	cout << endl;
 }
 
+//Boolean function that returns true if two values are in ascending order
+int ascending(int first, int second){
+	if(first <= second){
+		return true;
+	}
+	return false;
+}
+
+//Boolean function that returns true if two values are in descending order
+int descending(int first, int second){
+	if(first >= second){
+		return true;
+	}
+	return false;
+}
+
+//Bubble sort function, that can now sort in both ascending and descending order through the use of function pointers
+void bubble_sort(int *array, int size, int(*order)(int, int)){
+	bool sorted = false;
+	while(!sorted){
+		sorted = true;
+		for(int i = 0; i < size - 1; i++){
+			if(!(*order)(*(array + i), *(array + 1 + i))){
+				sorted = false;
+				int temp = *(array + i);
+				*(array + i) = *(array + i + 1);
+				*(array + i + 1) = temp;
+			}
+		}
+	}
+}
+
+//MAIN: used for testing methods
 int main(){
-	int * p = new int;
+	cout << "Testing bsort" << endl;
+	int * arr1 = new int;
 	cout << "Reading data.txt..." << endl;
-	int last = readData(p);
+	int size1 = readData(arr1);
 	cout << "Performing bubble sort..." << endl;
-	bsort(p, last);
-	writeToConsole(p, last);
+	bsort(arr1, size1);
+	writeToConsole(arr1, size1);
+	
+	cout << endl << "Testing bubble_sort with descending function pointer" << endl;
+	int * arr2 = new int;
+	cout << "Reading data.txt..." << endl;
+	int size2 = readData(arr2);
+	cout << "Performing bubble sort..." << endl;
+	bubble_sort(arr2, size2, descending);
+	writeToConsole(arr2, size2);
+	
+	cout << endl << "Testing bubble_sort with ascending function pointer" << endl;
+	int * arr3 = new int;
+	cout << "Reading data.txt..." << endl;
+	int size3 = readData(arr3);
+	cout << "Performing bubble sort..." << endl;
+	bubble_sort(arr3, size3, ascending);
+	writeToConsole(arr3, size3);
+	
+	
 	
 	return 0;
 }
